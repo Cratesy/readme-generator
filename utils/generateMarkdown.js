c5fa59225a7485272a83a81c8b392b2d0e0a9b7f
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-03 09:24:16 +0100

    added markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,54 @@ const renderLicenseSection = (license) => {};
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
-  return `# ${data.title}
+  return `# Project Title
 
+  ![mit license](https://img.shields.io/badge/license-MIT-green)
+
+  ## Description
+  
+  Project description goes here
+  
+  ## Table of Contents
+  
+    - [Project Title (question 1 - input)](#project-title-question-1---input)
+    - [Description](#description)
+    - [Table of Contents](#table-of-contents)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+  
+  ## Installation
+
+  \```
+  npm install
+  node index.js
+  \```
+  
+  ## Usage
+  
+  - When the app is running answer all questions
+  - After all questions view generated README markdown file
+  
+  ## License
+  
+  MIT License (question 2 - choices)
+  
+  ## Contributing
+
+  Open an issue or a pull request and I will sort it.
+  
+  ## Tests
+  
+  There are no tests for this app
+  
+  ## Questions
+  
+  - View my [GitHub](https://github.com/Cratesy) profile (question 8 - input)
+  - Email me at mike@email.com (question 9 - input)
 `;
 };
 
