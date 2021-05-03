9c493637339f0c167f7e02c7dff68027161760f3
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-03 09:24:46 +0100

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "Title",
+  },
+  {
+    type: "choice",
+    message: "what license?",
+    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Add a brief product description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do you install it?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "usage of the app?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "How to collaborate?",
+    name: "collaboration",
+  },
+  {
+    type: "input",
+    message: "Was there any tests?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Whats your github URL?",
+    name: "githubUrl",
+  },
+  {
+    type: "input",
+    message: "Whats your email address?",
+    name: "emailAddress",
+  },
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
