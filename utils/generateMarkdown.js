54f699aa49959cf89c4c5626c02350b4750d1490
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-05 13:08:13 +0100

    updated table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,15 +33,15 @@ const generateMarkdown = (answers) => {
   
   ## Table of Contents
   
-    - [${title}](#${title})
-    - [Description](#description)
-    - [Table of Contents](#table-of-contents)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
+  - [${title}](#${title})
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
   
   ## Installation
 
