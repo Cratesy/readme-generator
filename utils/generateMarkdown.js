f4f616b596de57e61d072d94fd0a516803efc1ab
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-05 13:49:16 +0100

    comments for rending license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) => {};
+const renderLicenseBadge = (license) => {
+  // <img
+  //   alt="AUR license badge"
+  //   src="https://img.shields.io/badge/license-Apache-blue"
+  // ></img>;
+  // <img
+  //   alt="APM badge"
+  //   src="https://img.shields.io/badge/license-MIT-green"
+  // ></img>;
+  // <img
+  //   alt="PyPI - License badge"
+  //   src="https://img.shields.io/badge/license-BSD-green"
+  // ></img>;
+  // <img
+  //   alt="Eclipse Marketplace badge"
+  //   src="https://img.shields.io/badge/license-GPL-blue"
+  // ></img>;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) => {};
+const renderLicenseLink = (license) => {
+  // https://img.shields.io/badge/license-${license}-<COLOR>
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = (license) => {};
 
-// TODO: Create a function to generate markdown for README
+//Function to generate markdown for README
 const generateMarkdown = (answers) => {
   const {
     title,
