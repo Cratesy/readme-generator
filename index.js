64b274b09e6feb889af9ea660d6282a7ea7ec5ac
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-05-16 20:43:31 +0100

    made fixes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //packages needed for this application
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
+
+const generateMarkdown = require("./utils/generateMarkdown");
 
 //array of questions for user input
 const questions = [
